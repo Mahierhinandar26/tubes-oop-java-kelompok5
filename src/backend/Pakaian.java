f454d2c8e69258b083036990276cc07708300c63
Mahierhinandar26 <mahierhinandar26@gmail.com>
2025-12-21 21:38:32 +0700

    Update backend src

--- src/backend/Pakaian.java
+++ src/backend/Pakaian.java
@@ -1,15 +1,13 @@
 package backend;
 
 public class Pakaian extends Produk {
-    
-    // Constructor dengan ID (untuk testing/import data)
+
     public Pakaian(int id, String nama, double harga, int stok) {
-        super(id, nama, harga, stok, "Pakaian");
+        super(id, nama, harga, stok);
     }
-    
+
     @Override
-    public void info() {
-        System.out.printf("%d. %s | Pakaian | Rp%,.0f | Stok: %d%n", 
-            id, nama, harga, stok);
+    public String getKategori() {
+        return "Pakaian";
     }
 }
--- src/backend/StokHabisException.java
+++ src/backend/StokHabisException.java
@@ -1,8 +1,6 @@
 package backend;
-
 public class StokHabisException extends Exception {
-    public StokHabisException(String produk, int tersedia, int diminta) {
-        super(String.format("Stok %s tidak cukup! Tersedia: %d, Diminta: %d", 
-            produk, tersedia, diminta));
+    public StokHabisException(String pesan) {
+        super(pesan);
     }
 }
